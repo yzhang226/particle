a6f652a97966aa651117acea97dd8b6e9c73da8a
zhangying <zhangying@hujiang.com>
2018-12-24 22:54:36 +0800

    update: path for win

--- particle-plugin-javabean/src/main/java/org/lightning/particle/plugin/javabean/utils/PathUtils.java
+++ particle-plugin-javabean/src/main/java/org/lightning/particle/plugin/javabean/utils/PathUtils.java
@@ -1,5 +1,7 @@
 package org.lightning.particle.plugin.javabean.utils;
 
+import org.apache.commons.lang3.SystemUtils;
+
 /**
  * Created by cook on 2018/12/21
  */
@@ -7,12 +9,26 @@ public abstract class PathUtils {
 
     public static String getTopDir(Class<?> clazz, String currentArtifactId, String resourcePath) {
         String propPath = clazz.getClassLoader().getResource(resourcePath).getPath();
+
+        propPath = trimPath(propPath);
+
         System.out.println("propPath is " + propPath);
 
         String topDir = propPath.substring(0, propPath.indexOf("/" + currentArtifactId));
         System.out.println("topDir is " + topDir);
 
         return topDir;
+    }
+
+    private static String trimPath(String propPath) {
+        if (SystemUtils.IS_OS_WINDOWS) {
+            int idx2 = propPath.indexOf('/', 1);
+            String diskChar = propPath.substring(1, idx2);
+            if (diskChar.contains(":")) {
+                propPath = propPath.substring(1);
+            }
+        }
+        return propPath;
     }
 
 
